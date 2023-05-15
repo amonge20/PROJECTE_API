18a3dfc21a01bb2070ff71bad9d65f2714c6893b
amonge20 <amonge20@jaumebalmes.net>
2023-05-15 20:06:51 +0200

    ARA FENT UN TRIVIAL

--- script.js
+++ script.js
@@ -1,37 +1,69 @@
-// JavaScript para consumir la API de Instant Gaming
-const gameList = document.getElementById('gameList');
+// script.js
+const questionElement = document.getElementById('question');
+const optionsElement = document.getElementById('options');
 
-fetch('https://instant-gaming-api.p.rapidapi.com/api/v1/instantGaming', {
-  headers: {
-    'X-RapidAPI-Key': 'ebf6ef4972mshd45adee9190a720p117553jsn6544d26b5618',
-    'X-RapidAPI-Host': 'instant-gaming-api.p.rapidapi.com'
-  },
-  params: {
-    name: 'stardew valley',
-    platform: 'pc'
-  }
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error al obtener los datos');
-    }
-    return response.json();
-  })
-  .then(data => {
-    data.forEach(game => {
-      const li = document.createElement('li');
-      const h2 = document.createElement('h2');
-      const p = document.createElement('p');
-      h2.textContent = game.title;
-      p.textContent = game.platform;
-      li.appendChild(h2);
-      li.appendChild(p);
-      gameList.appendChild(li);
+function displayQuestion(question) {
+  questionElement.textContent = question.question;
+
+  optionsElement.innerHTML = '';
+
+  question.options.forEach(option => {
+    const optionElement = document.createElement('div');
+    optionElement.classList.add('option');
+    optionElement.textContent = option;
+    optionElement.addEventListener('click', () => {
+      checkAnswer(option, question.answer);
     });
-  })
-  .catch(error => {
-    console.error(error);
-    const errorMessage = document.createElement('p');
-    errorMessage.textContent = 'Error al obtener los datos';
-    gameList.appendChild(errorMessage);
+
+    optionsElement.appendChild(optionElement);
   });
+}
+
+function checkAnswer(selectedOption, correctAnswer) {
+  optionsElement.innerHTML = '';
+
+  const resultElement = document.createElement('p');
+  resultElement.textContent = selectedOption === correctAnswer ? '¡Respuesta correcta!' : 'Respuesta incorrecta';
+  resultElement.style.margin = '20px 0';
+
+  optionsElement.appendChild(resultElement);
+
+  setTimeout(getRandomQuestion, 2000);
+}
+
+function getRandomQuestion() {
+  axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
+    .then(response => {
+      const data = response.data.results[0];
+      const question = {
+        question: decodeEntities(data.question),
+        options: shuffleOptions([
+          decodeEntities(data.correct_answer),
+          ...data.incorrect_answers.map(decodeEntities)
+        ]),
+        answer: decodeEntities(data.correct_answer)
+      };
+
+      displayQuestion(question);
+    })
+    .catch(error => {
+      console.error(error);
+      questionElement.textContent = 'Error al obtener la pregunta';
+    });
+}
+
+function decodeEntities(text) {
+  const textarea = document.createElement('textarea');
+  textarea.innerHTML = text;
+  return textarea.value;
+}
+
+function shuffleOptions(options) {
+  for (let i = options.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [options[i], options[j]] = [options[j], options[i]];
+  }
+  return options;
+}
+
+getRandomQuestion();

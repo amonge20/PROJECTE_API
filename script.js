1702f3ce496b2c15be50384ad14c6a0d2d9dfe2b
amonge20 <amonge20@jaumebalmes.net>
2023-05-17 14:07:25 +0200

    api de pelicula

--- script.js
+++ script.js
@@ -1,69 +1,77 @@
-// script.js
-const questionElement = document.getElementById('question');
-const optionsElement = document.getElementById('options');
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
 
-function displayQuestion(question) {
-  questionElement.textContent = question.question;
+btnSiguiente.addEventListener('click', () => {
+	if(pagina < 1000){
+		pagina += 1;
+		cargarPeliculas();
+	}
+});
 
-  optionsElement.innerHTML = '';
+btnAnterior.addEventListener('click', () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPeliculas();
+	}
+});
 
-  question.options.forEach(option => {
-    const optionElement = document.createElement('div');
-    optionElement.classList.add('option');
-    optionElement.textContent = option;
-    optionElement.addEventListener('click', () => {
-      checkAnswer(option, question.answer);
-    });
+const cargarPeliculas = async() => {
+	try {
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
+	
+		console.log(respuesta);
 
-    optionsElement.appendChild(optionElement);
+		// Si la respuesta es correcta
+		if(respuesta.status === 200){
+			const datos = await respuesta.json();
+			
+			let peliculas = '';
+			datos.results.forEach(pelicula => {
+				peliculas += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+						<h3 class="titulo">${pelicula.title}</h3>
+					</div>
+				`;
+			});
+
+			document.getElementById('contenedor').innerHTML = peliculas;
+
+		} else if(respuesta.status === 401){
+			console.log('Pusiste la llave mal');
+		} else if(respuesta.status === 404){
+			console.log('La pelicula que buscas no existe');
+		} else {
+			console.log('Hubo un error y no sabemos que paso');
+		}
+
+	} catch(error){
+		console.log(error);
+	}
+
+}
+
+cargarPeliculas();
+
+$(document).ready(function() {
+	// Manejo del evento de envío del formulario
+	$("#movieForm").submit(function(event) {
+	  event.preventDefault();
+	  var movieId = $("#movieId").val();
+
+	  // Realiza una solicitud a tu API para obtener el resumen de la película
+	  $.ajax({
+		url: "/api/movies/" + movieId, // Reemplaza esta URL con la ruta de tu API
+		method: "GET",
+		success: function(response) {
+		  // Muestra el resumen de la película en el contenedor correspondiente
+		  $("#movieSummary").html(response.summary);
+		},
+		error: function() {
+		  // Manejo del error en caso de que la solicitud falle
+		  $("#movieSummary").html("No se pudo cargar el resumen de la película.");
+		}
+	  });
+	});
   });
-}
-
-function checkAnswer(selectedOption, correctAnswer) {
-  optionsElement.innerHTML = '';
-
-  const resultElement = document.createElement('p');
-  resultElement.textContent = selectedOption === correctAnswer ? '¡Respuesta correcta!' : 'Respuesta incorrecta';
-  resultElement.style.margin = '20px 0';
-
-  optionsElement.appendChild(resultElement);
-
-  setTimeout(getRandomQuestion, 2000);
-}
-
-function getRandomQuestion() {
-  axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
-    .then(response => {
-      const data = response.data.results[0];
-      const question = {
-        question: decodeEntities(data.question),
-        options: shuffleOptions([
-          decodeEntities(data.correct_answer),
-          ...data.incorrect_answers.map(decodeEntities)
-        ]),
-        answer: decodeEntities(data.correct_answer)
-      };
-
-      displayQuestion(question);
-    })
-    .catch(error => {
-      console.error(error);
-      questionElement.textContent = 'Error al obtener la pregunta';
-    });
-}
-
-function decodeEntities(text) {
-  const textarea = document.createElement('textarea');
-  textarea.innerHTML = text;
-  return textarea.value;
-}
-
-function shuffleOptions(options) {
-  for (let i = options.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [options[i], options[j]] = [options[j], options[i]];
-  }
-  return options;
-}
-
-getRandomQuestion();
